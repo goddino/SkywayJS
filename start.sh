63e0fccb6cd14b94c1036e397beb668a4f906e39
Xiangrong <goddino@gmail.com>
2014-07-08 13:05:22 +0800

    AND-17 WIP: Set up local web client. Created local web client by copying SkywayJS demo/app-1.html and changing the key id to ce19ec98-f618-4eed-afee-cf26a80640df, as given by registering app at http://developer.temasys.com.sg/ with the following parameters: Description: xrWeb Full domain Name: localhost CORS domainName: localhost CallBack URL: localhost/usage

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-open -a /Applications/Google\ Chrome.app http://localhost:8081/demo/index.html;
-
+# open -a /Applications/Google\ Chrome.app http://localhost:8081/demo/index.html;
+open -a /Applications/Google\ Chrome.app http://localhost:8081/demo/SkywayClient.html;
