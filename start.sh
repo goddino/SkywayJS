ab518bd38228753f53ee72810190947d75c306dc
Xiangrong <goddino@gmail.com>
2014-08-09 23:07:31 +0800

    Added adapter.js line for compatibility on Windows.

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 # open -a /Applications/Google\ Chrome.app http://localhost:8081/demo/index.html;
+# open -a "/C/Program Files (x86)/Google/Chrome/Application/chrome.exe" http://localhost:8081/demo/SkywayClient.html;
 open -a /Applications/Google\ Chrome.app http://localhost:8081/demo/SkywayClient.html;
